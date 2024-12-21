e57d1e049d2a30979834e57bf2d78a6c58bdb1ed
sm4rtdev <elonscottdev@gmail.com>
2024-12-21 07:40:34 -0500

    Update state.rs

--- src/state.rs
+++ src/state.rs
@@ -2,7 +2,7 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 use cosmwasm_std::{Addr, Coin, Uint128};
-use cw_storage_plus::Item;
+use cw_storage_plus::Item;         
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct State {
@@ -11,5 +11,5 @@ pub struct State {
     pub price: Coin,
     pub balance: Uint128,
 }
-                                   
+                                                                      
 pub const STATE: Item<State> = Item::new("state");
